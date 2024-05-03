8d5c1acaecbe51c82e01e850ee56fc4c1e306ce2
Roydamaizal <168014225+Roydamaizal@users.noreply.github.com>
2024-05-03 17:49:51 +0700

    Add files via upload

---
+++ EqualString.java
@@ -0,0 +1,10 @@
+public class EqualString {
+    public static void main(String[] args) {
+        String string1 = "roi";
+        String string2 = "roi";
+        String string3 = "Roi";
+        System.out.println("Hasilnya:" + string1.equals(string2));
+        System.out.println("Hasilnya:" + string1.equals(string3));
+    }
+
+}
